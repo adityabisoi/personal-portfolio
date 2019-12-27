3338a45920a48900c5a1035d6f272721ea585318
adityabisoi <adityabisoi1999@gmail.com>
2019-12-27 17:32:20 +0530

    requirements.txt added

---
+++ blog/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+urlpatterns = [
+    path('', jobs.views.home),
+] 
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,4 +22,5 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
