8131dab289296e426eedd8ad850289439d0f2e9a
adityabisoi <adityabisoi1999@gmail.com>
2019-12-30 12:04:22 +0530

    buggy commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'portfolio/static/')
-]
+# STATICFILES_DIR = [
+#     os.path.join(BASE_DIR, 'portfolio/static/')
+# ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home),
     path('blog/', include('blog.urls')),
-] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
