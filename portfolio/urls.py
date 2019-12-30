ad172d0f1337d99e0079e320cfae8a3c4d31dbb8
adityabisoi <adityabisoi1999@gmail.com>
2019-12-30 11:56:21 +0530

    static files added

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home),
     path('blog/', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
